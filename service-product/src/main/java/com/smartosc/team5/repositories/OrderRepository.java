7fa66bd36a02a35c4ff97b173782843d139599d7
[huupd@smartosc.com] <huupd@smartosc.com>
2020-06-05 09:58:32 +0700

    order repository

--- service-product/src/main/java/com/smartosc/team5/repositories/OrderRepository.java
+++ service-product/src/main/java/com/smartosc/team5/repositories/OrderRepository.java
@@ -2,11 +2,8 @@ package com.smartosc.team5.repositories;
 
 import com.smartosc.team5.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 /**
  * Team5
